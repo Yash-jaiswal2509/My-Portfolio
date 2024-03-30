ced0da479de76b67af3a599c8422e6637a64884c
Yash-jaiswal2509 <yashjaiswal2509@gmail.com>
2024-03-30 11:09:10 +0530

    changed fetch projects

--- backend/src/controllers/projects.controller.ts
+++ backend/src/controllers/projects.controller.ts
@@ -52,17 +52,29 @@ export const addProject = asyncHandler(async (req: Request, res: Response) => {
     .json(new apiResponse(200, createdProject, "Project added successfully"));
 });
 
-
 export const fetchProjects = asyncHandler(
   async (req: Request, res: Response) => {
-    const projects = await Project.find();
+    try {
+      const projects = await Project.find();
 
-    if (!projects) {
-      throw new Error("Not able to fetch the projects");
+      if (!projects || projects.length === 0) {
+        res.status(404).json(new apiResponse(404, null, "No projects found"));
+      }
+
+      res
+        .status(200)
+        .json(new apiResponse(200, projects, "Projects fetched successfully"));
+    } catch (error) {
+      console.error("Error fetching projects:", error);
+      res
+        .status(500)
+        .json(
+          new apiResponse(
+            500,
+            null,
+            "Failed to fetch projects. Please try again later."
+          )
+        );
     }
-
-    res
-      .status(201)
-      .json(new apiResponse(200, projects, "Projecs fetched succesfully"));
   }
 );
