65f239b92b101171e44557eab2b81f87c79b34d1
Yash-jaiswal2509 <yashjaiswal2509@gmail.com>
2024-03-30 10:44:12 +0530

    change api client

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -70,11 +70,7 @@ export const fetchProjects = async () => {
     const response = await axios.get("api/v1/projects", {
       withCredentials: true,
     });
-    if (response.status === 200) {
-      return response;
-    } else {
-      throw new Error("Unexpected response status: " + response.status);
-    }
+    return response;
   } catch (error) {
     console.error("Error fetching projects:", error);
     throw new Error("Failed to fetch projects. Please try again later.");
